9e8b7ebc9635f735076bcfd5b1b6a602a3633a38
Brendan Doyle <brendanwex@outlook.com>
2017-06-09 20:24:01 +0100

    Change Copyright

--- system/Controller.php
+++ system/Controller.php
@@ -2,8 +2,7 @@
 
 /**
  * @User Brendan
- * @Company Graphedia.com
- * @Package gr-framework
+ * @Package custom-php-framework
  * @File controller.class.php
  * @Date 29-Mar-17  2:15 PM
  * @Version
